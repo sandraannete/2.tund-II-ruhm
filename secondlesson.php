704d325a7cb9a5f8475499e6a4d798e5adb1aea7
sandtaga <sandtaga@greeny.cs.tlu.ee>
2016-09-13 13:10:24 +0300

    added for loop and if statement

---
+++ secondlesson.php
@@ -0,0 +1,32 @@
+<?php
+
+	$age = 5;
+	
+	if ($age < 18) {
+		echo "alaealine";
+		
+	} else {
+		
+		echo "täisealine";
+	
+	}
+
+
+?>
+
+<br>
+
+<?php
+
+	//eesmärk vanusele vastavalt
+	// trükime välja 'palju'
+	
+	for ( $i = 1; $i <= $age ; $i = $i + 1) {
+		
+		echo $i."palju ";
+		
+	}
+	
+	echo "õnne!";
+	
+?>
